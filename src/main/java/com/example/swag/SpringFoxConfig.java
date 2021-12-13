9edda6beaf1c7f89c3f275b274ddaebc73b3d633
Fisher <alexfisher13@yandex.ru>
2021-12-13 15:49:33 +0300

    added swagger

---
+++ src/main/java/com/example/swag/SpringFoxConfig.java
@@ -0,0 +1,27 @@
+package com.example.swag;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2) //Docket - настройки swagger (тип доки Swagger2)
+                .select()
+                //указываем базовый пакет с контроллерами
+                .apis(RequestHandlerSelectors.basePackage("com.example.swag.controller"))
+                //выбираем все эндпоинты которые найдем
+                .paths(PathSelectors.any())
+                .build();
+
+                //http://localhost:8080/swagger-ui/
+    }
+}
+
--- src/main/java/com/example/swag/controller/ApiController.java
+++ src/main/java/com/example/swag/controller/ApiController.java
@@ -1,20 +1,25 @@
-package com.example.swag;
+package com.example.swag.controller;
 
 import com.example.swag.model.Client;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api("Самый замечательный API")
 public class ApiController {
 
     @GetMapping(value = "/getClient")
+    @ApiOperation("Получение клиента")
     public Client getClient() {
         return new Client("Sasha",  29, true);
     }
 
     @PostMapping(value = "/addClient")
+    @ApiOperation("Добавлние клиента")
     public String addClient(@RequestBody Client client) {
         return String.format("Client %s was added", client.getName());
     }
